b4f3971c03a766201bb7b224910a35c8df2008bb
brendagold <petersbrenda47@gmail.com>
2022-05-16 18:51:22 +0800

    solved the largest difference problem and remove duplicates

--- LargestDifference.js
+++ LargestDifference.js
@@ -1,2 +1,26 @@
 // Given an array of integers, find the largest difference between two elements such that the element of lesser value must come before the greater element
 
+const difference = (nums) => {
+    let length = nums.length
+    // using Kadane’s approach, i assumed that the initial different is the first two elements.
+    let max_difference = nums[1] - nums[0]
+
+    let min_element = nums[0]
+
+    for(let i = 0; i < length; i++) {
+        // iterating through the array and getting the max different 
+        if (nums[i] - min_element > max_difference) {
+            max_difference = nums[i] - min_element
+        }
+        // getting the value for the min_element
+        if (nums[i] < min_element) {
+            min_element = nums[i]
+        }
+    }
+    return max_difference
+    
+}
+
+let nums = [ 18,2,8,1,2,3,2,6]
+
+console.log(difference(nums))
--- RemoveDuplicates.js
+++ RemoveDuplicates.js
@@ -1,10 +1,31 @@
 // Removing duplicates of an array and returning an array of only unique elements
 
-let nums = [1.2,3,2,4,1,5,]
+// first approach was to use javascript array methods to get the solution
+// such as nums.filter((item, index) => nums.indexOf(item) === index)
+
+// Second approach is using two loops to create a new array of unique elements but it is not efficient 
+// because its time complexity is O(n^2) 
+
+// Let me solve the question using hashmap, its time complexity is O(n) and space complexity is O(n)
+// here our key will be the number and our value will be true or false. 
+
+let nums = [1,6, 8, 6,9, 7, 9, 3,2,3,2,4,1,5,]
 
 const removeDuplicates = (nums) => {
-    let newArr = []
+    
+    let uniqueArr = []
+    let hashMap = {}
     for(let i =0; i < nums.length; i++) {
-
+        // if the element exists in the hashmap, continue to the next i
+        if (hashMap[nums[i]]) {
+            continue
+        } else {
+            //else add it to the array of unique values and set its value in the hashmap as true.
+            uniqueArr.push(nums[i])
+            hashMap[nums[i]] = true
+        }
     }
+     return uniqueArr
 }
+
+console.log(removeDuplicates(nums))
